f3c24751347dda5e722903a275d18133e309d771
rick <technoweenie@gmail.com>
2011-03-07 12:37:30 -0800

    store shas of paragraph contents

--- lib/email_reply_parser.rb
+++ lib/email_reply_parser.rb
@@ -1,16 +1,21 @@
 require 'strscan'
+require 'digest/sha1'
 
 class EmailReplyParser
   class Block
-    attr_reader :levels
+    attr_reader :levels, :shas, :repeated, :lines
 
     def initialize(levels = 0)
       @levels = levels
       @lines  = []
       @joined = nil
+      @shas   = {}  # sha => {:start => 0, :end => 1}
+      @sha    = nil # current sha
+      @line   = -1  # current line number
     end
 
     def <<(line)
+      update_paragraph_sha(line)
       @lines << line
     end
 
@@ -22,8 +27,39 @@ class EmailReplyParser
       @joined ||= @lines.join("\n")
     end
 
+    def finish
+      end_sha
+    end
+
     def inspect
       to_s.inspect
+    end
+
+  private
+    def update_paragraph_sha(line)
+      @line   += 1
+      stripped = line.sub(/^(\s|>)+/, '')
+      if stripped.size.zero?
+        end_sha(@line-1)
+      else
+        start_sha
+        @sha.update(stripped)
+      end
+    end
+
+    def start_sha
+      if @sha.nil?
+        @shas[:current] = {:start => @line}
+      end
+      @sha ||= Digest::SHA1.new
+    end
+
+    def end_sha(ending_line = @line)
+      if @sha
+        @shas[@sha.hexdigest] = @shas.delete(:current).
+          update(:end => ending_line)
+      end
+      @sha = nil
     end
   end
 
@@ -39,10 +75,12 @@ public
         line_levels = $1.size
       end
       if !block || block.levels != line_levels
+        block.finish if block
         blocks << (block = Block.new(line_levels))
       end
       block << line
     end
+    blocks.last.finish if blocks.size > 0
     blocks
   end
 end
--- test/email_reply_parser_test.rb
+++ test/email_reply_parser_test.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'test/unit'
 require 'pathname'
+require 'pp'
 
 dir = Pathname.new File.expand_path(File.dirname(__FILE__))
 require dir + '..' + 'lib' + 'email_reply_parser'
@@ -9,24 +10,34 @@ EMAIL_FIXTURE_PATH = dir + 'emails'
 
 class EmailReplyParserTest < Test::Unit::TestCase
   def test_reads_simple_body
-    body = email :email_1_1
+    body   = email :email_1_1
     blocks = EmailReplyParser.scan(body)
     assert_equal 1, blocks.size
     assert_match /Hi folks/, blocks.first.to_s
   end
 
   def test_reads_bottom_poster
-    body = email :email_1_2
+    body   = email :email_1_2
     blocks = EmailReplyParser.scan(body)
     assert_equal 5, blocks.size
     assert_equal [0,1,0,1,0], blocks.map { |b| b.levels }
   end
 
   def test_reads_multi_level_replies
-    body = email :email_1_3
+    body   = email :email_1_3
     blocks = EmailReplyParser.scan(body)
     assert_equal 7, blocks.size
     assert_equal [0,1,2,1,2,1,0], blocks.map { |b| b.levels }
+  end
+
+  def test_stores_shas_of_blocks
+    body   = email :email_1_3
+    blocks = EmailReplyParser.scan(body)
+    assert_equal 19, blocks.inject(0) { |n, b| n + b.shas.size }
+    assert_equal 1,  blocks[6].shas.size
+    blocks[6].shas.each do |sha, hash|
+      assert blocks[4].shas.key?(sha)
+    end
   end
 
   def email(name)
