2d9bd23cd5076fc85a606bedaf9159217bf6b26a
rick <technoweenie@gmail.com>
2011-03-08 15:03:46 -0800

    Rename Block#reply? to Block#quoted?  -- also: tomdoc'd

--- lib/email_reply_parser.rb
+++ lib/email_reply_parser.rb
@@ -5,10 +5,22 @@ require 'set'
 class EmailReplyParser
   VERSION = "0.1.0"
 
+  # Parses an email body, pulling out quoted regions and finding paragraphs
+  # that should be hidden.  This is designed to be called on a sequence of
+  # emails.
+  #
+  # body - String body of the email.
+  # shas - Optional Set of SHA1 hex values that match the content of
+  #        paragraphs.  This is used to check if any paragraphs in this
+  #        reply are repeated from earlier emails in this thread.
+  #
+  # Returns a Reply instance.
   def self.read(body, shas = nil)
     Reply.new(body, shas)
   end
 
+  # Represents a parsed email body.  Each email is broken up into Block
+  # instances, according to the quote level.
   class Reply
     attr_reader :blocks, :shas
 
@@ -39,6 +51,13 @@ class EmailReplyParser
     end
 
   private
+    # Scans each incoming line.  If the quote level changes, create a new
+    # instance.  The quote level is determined by counting the number of '>'
+    # characters at the beginning of a string.
+    #
+    # line - String line of text from the email.
+    #
+    # Returns nothing.
     def scan_line(line)
       line.rstrip!
       line_levels = 0
@@ -54,6 +73,9 @@ class EmailReplyParser
       @block << line
     end
 
+    # Determines which blocks should be hidden.  We want to hide any quoted
+    # regions or signatures at the bottom.  Quick checking as soon as visible
+    # text is found.
     def scan_for_hidden
       @blocks.reverse.each do |block|
         if block.paragraphs.empty?
@@ -61,7 +83,7 @@ class EmailReplyParser
         else
           block.paragraphs.reverse.each do |para|
             if !(para.is_hidden = (
-                  block.reply? ||
+                  block.quoted? ||
                   (para.signature? && @current_shas.include?(para.sha))
                 ))
               return
@@ -72,6 +94,8 @@ class EmailReplyParser
     end
   end
 
+  # Represents a quoted portion of the email.  A Block has 0 or more Paragraph
+  # instances.
   class Block
     attr_reader :levels
 
@@ -86,10 +110,19 @@ class EmailReplyParser
       @hidden = nil
     end
 
+    # Sets this Block to be hidden.  Any blocks with no actual content are 
+    # marked as hidden.
+    #
+    # bool - Either a true or false.
+    #
+    # Returns nothing.
     def is_hidden=(bool)
       @hidden = !!bool
     end
 
+    # Public: Gets whether this Block should be hidden or not.
+    #
+    # Returns true if this Block should be hidden, or false.
     def hidden?
       if @hidden.nil?
         @hidden = (p = @paras.first and p.hidden?) || false
@@ -97,36 +130,67 @@ class EmailReplyParser
       @hidden
     end
 
-    def reply?
+    # Public: Gets whether this Block has been quoted at least once.
+    #
+    # Returns true if the Block has been quoted, or false.
+    def quoted?
       @levels > 0
     end
 
+    # Public: Gets paragraphs for this Block.
+    #
+    # Returns an Array of Paragraph instances.
     def paragraphs
       @paras
     end
 
+    # Public: Adds the given line to this block.
+    #
+    # line - String line of text from the email.
+    #
+    # Returns nothing.
     def <<(line)
       update_paragraph(line)
       @lines << line
     end
 
+    # Public: Checks whether the SHA1 hex occurs in this Block.
+    #
+    # sha - String SHA1 hex value.
+    #
+    # Returns true if the SHA1 occurs, or false.
     def include?(sha)
       @shas.include?(sha)
     end
 
+    # Public: Gets the full content for this Block.
+    #
+    # Returns a String.
     def to_s
       @joined ||= @lines.join("\n")
     end
 
-    def finish
-      end_para
-    end
-
+    # Public: Gets the content of this Block for inspection.
+    #
+    # Returns a String.
     def inspect
       to_s.inspect
     end
 
+    # Finishes up any loose ends after adding the last line of text to this
+    # Block.
+    #
+    # Returns nothing.
+    def finish
+      end_para
+    end
+
   private
+    # Updates the current Paragraph for this Block during the parsing.
+    #
+    # line - String line of text from the email.
+    #
+    # Returns nothing
     def update_paragraph(line)
       @line   += 1
       stripped = line.sub(/^(\s|>)+/, '')
@@ -144,10 +208,19 @@ class EmailReplyParser
       end
     end
 
+    # Starts a new Paragraph during the parsing.
+    #
+    # Returns nothing.
     def start_para
       @paras << (@para = Paragraph.new(@line))
     end
 
+    # Finishes the current Paragraph and adds its SHA1 hash to this Block's
+    # Set of SHAs.
+    #
+    # ending_line - String line of text from the email.
+    #
+    # Returns nothing.
     def end_para(ending_line = @line)
       return if !@para
       @shas << @para.finish(ending_line).sha
@@ -155,6 +228,11 @@ class EmailReplyParser
     end
   end
 
+  # Tracks each paragraph of content in the email and builds up a SHA1 hash of
+  # the contents.  This SHA1 hash is used to determine if this paragraph is
+  # repeated in further emails in the current thread.  Paragraphs don't store
+  # the actual content, but contain start/end line numbers that match lines in
+  # the Paragraph's Block instance.
   class Paragraph < Struct.new(:start, :end, :sha, :is_signature, :is_hidden)
     def initialize(s = 0)
       super
@@ -163,18 +241,38 @@ class EmailReplyParser
       self.is_hidden    = false
     end
 
+    # Public: Gets whether this Paragraph is considered an email signature.
+    # Email signatures are just paragraphs that start with a leading dash or
+    # underscore.
+    #
+    # Returns true if this Paragraph is a signature, or false.
     def signature?
       is_signature
     end
 
+    # Public: Gets whether this Paragraph is thought to be hidden.  This is
+    # determined after each line of text in the email has been processed.
+    #
+    # Returns true if this Paragraph is hidden, or false.
     def hidden?
       is_hidden
     end
 
+    # Updates the SHA1 hash of this paragraph.
+    #
+    # s - String line of text from the email.
+    #
+    # Returns nothing.
     def update(s)
       self.sha.update(s)
     end
 
+    # Sets the ending line of this Paragraph, and finalizes the SHA1 hash.
+    #
+    # ending_line - Fixnum representing which line in the Block this paragraph
+    #               ended on.
+    #
+    # Returns nothing.
     def finish(ending_line)
       self.end = ending_line
       self.sha = self.sha.to_s
