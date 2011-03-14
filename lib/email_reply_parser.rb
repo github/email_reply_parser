b7b7dcae645dc8388b9ecbfea71314f0ed40f3c3
rick <technoweenie@gmail.com>
2011-03-14 14:30:35 -0700

    completely rewrite the parser * all signatures at the bottom are hidden * the "on <date>, <author> wrote:" text above replies is counted as part of the reply

--- lib/email_reply_parser.rb
+++ lib/email_reply_parser.rb
@@ -1,41 +1,24 @@
 require 'strscan'
-require 'digest/sha1'
-require 'set'
 
 class EmailReplyParser
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
 
-  # Parses an email body, pulling out quoted regions and finding paragraphs
-  # that should be hidden.  This is designed to be called on a sequence of
-  # emails.
-  #
-  # body - String body of the email.
-  # shas - Optional Set of SHA1 hex values that match the content of
-  #        paragraphs.  This is used to check if any paragraphs in this
-  #        reply are repeated from earlier emails in this thread.
-  #
-  # Returns a Reply instance.
-  def self.read(body, shas = nil)
-    Reply.new(body, shas)
+  def self.read(text)
+    Email.new.read(text)
   end
 
-  # Represents a parsed email body.  Each email is broken up into Block
-  # instances, according to the quote level.
-  class Reply
-    attr_reader :blocks, :shas
+  class Email
+    attr_reader :fragments
 
-    def initialize(text, shas = nil)
-      @blocks = []
-      @block  = nil
+    def initialize
+      @fragments = []
+    end
 
-      # add this reply's shas to the given shas Set
-      @shas = shas || Set.new
-
-      # check if this reply's blocks are hidden with with the unmodified
-      # sha Set.  This reply's own repeat blocks can't mark itself as hidden.
-      @current_shas = @shas.dup
-
-      @scanner = StringScanner.new(text)
+    def read(text)
+      text.reverse!
+      @found_visible = false
+      @fragment = nil
+      @scanner  = StringScanner.new(text)
       while line = @scanner.scan_until(/\n/)
         scan_line(line)
       end
@@ -44,239 +27,84 @@ class EmailReplyParser
         scan_line(last_line)
       end
 
-      @blocks.last.finish if @blocks.size > 0
+      finish_fragment
 
-      scan_for_hidden
-      @block = @scanner = @current_shas = nil
+      @scanner = @fragment = nil
+      @fragments.reverse!
+      self
     end
 
   private
-    # Scans each incoming line.  If the quote level changes, create a new
-    # instance.  The quote level is determined by counting the number of '>'
-    # characters at the beginning of a string.
-    #
-    # line - String line of text from the email.
-    #
-    # Returns nothing.
+    EMPTY = "".freeze
     def scan_line(line)
-      line.rstrip!
-      line_levels = 0
-      if line =~ /^(>+)/
-        line_levels = $1.size
+      line.chomp!("\n")
+      line.lstrip!
+      line_levels = line =~ /(>+)$/ ? $1.size : 0
+
+      if @fragment && line == EMPTY
+        if @fragment.lines.last =~ /[\-\_]$/
+          @fragment.signature = true
+          finish_fragment
+        end
       end
-      if !@block || @block.levels != line_levels
-        @block.finish if @block
-        @blocks << (@block = Block.new(
-          :levels => line_levels,
-          :shas   => @shas))
+
+      if @fragment &&
+          ((@fragment.quoted? != line_levels.zero?) ||
+           (@fragment.quoted? && quote_header?(line)))
+        @fragment.lines << line
+      else
+        finish_fragment
+        @fragment = Fragment.new(!line_levels.zero?, line)
       end
-      @block << line
     end
 
-    # Determines which blocks should be hidden.  We want to hide any quoted
-    # regions or signatures at the bottom.  Quick checking as soon as visible
-    # text is found.
-    def scan_for_hidden
-      @blocks.reverse.each do |block|
-        if block.paragraphs.empty?
-          block.is_hidden = true
-        else
-          block.paragraphs.reverse.each do |para|
-            if !(para.is_hidden = (
-                  block.quoted? ||
-                  (para.signature? && @current_shas.include?(para.sha))
-                ))
-              return
-            end
+    def quote_header?(line)
+      line =~ /^:etorw.*nO$/
+    end
+
+    def finish_fragment
+      if @fragment
+        @fragment.finish
+        if !@found_visible
+          if @fragment.quoted? || @fragment.signature? ||
+              @fragment.to_s.strip == EMPTY
+            @fragment.hidden = true
+          else
+            @found_visible = true
           end
         end
+        @fragments << @fragment
       end
+      @fragment = nil
     end
   end
 
-  # Represents a quoted portion of the email.  A Block has 0 or more Paragraph
-  # instances.
-  class Block
-    attr_reader :levels
+  class Fragment < Struct.new(:quoted, :signature, :hidden)
+    attr_reader :lines, :content
 
-    def initialize(options = {})
-      @levels = options[:levels].to_i
-      @lines  = []
-      @shas   = options[:shas] || Set.new
-      @paras  = []
-      @line   = -1  # current line number
-      @para   = nil # current paragraph
-      @joined = nil
-      @hidden = nil
+    def initialize(quoted, first_line)
+      self.signature = self.hidden = false
+      self.quoted = quoted
+      @lines      = [first_line]
+      @content    = nil
+      @lines.compact!
     end
 
-    # Sets this Block to be hidden.  Any blocks with no actual content are 
-    # marked as hidden.
-    #
-    # bool - Either a true or false.
-    #
-    # Returns nothing.
-    def is_hidden=(bool)
-      @hidden = !!bool
+    alias quoted?    quoted
+    alias signature? signature
+    alias hidden?    hidden
+
+    def finish
+      @content = @lines.join("\n")
+      @content.reverse!
     end
 
-    # Public: Gets whether this Block should be hidden or not.
-    #
-    # Returns true if this Block should be hidden, or false.
-    def hidden?
-      if @hidden.nil?
-        @hidden = (p = @paras.first and p.hidden?) || false
-      end
-      @hidden
-    end
-
-    # Public: Gets whether this Block has been quoted at least once.
-    #
-    # Returns true if the Block has been quoted, or false.
-    def quoted?
-      @levels > 0
-    end
-
-    # Public: Gets paragraphs for this Block.
-    #
-    # Returns an Array of Paragraph instances.
-    def paragraphs
-      @paras
-    end
-
-    # Public: Adds the given line to this block.
-    #
-    # line - String line of text from the email.
-    #
-    # Returns nothing.
-    def <<(line)
-      update_paragraph(line)
-      @lines << line
-    end
-
-    # Public: Checks whether the SHA1 hex occurs in this Block.
-    #
-    # sha - String SHA1 hex value.
-    #
-    # Returns true if the SHA1 occurs, or false.
-    def include?(sha)
-      @shas.include?(sha)
-    end
-
-    # Public: Gets the full content for this Block.
-    #
-    # Returns a String.
     def to_s
-      @joined ||= @lines.join("\n")
+      @content
     end
 
-    # Public: Gets the content of this Block for inspection.
-    #
-    # Returns a String.
     def inspect
       to_s.inspect
-    end
-
-    # Finishes up any loose ends after adding the last line of text to this
-    # Block.
-    #
-    # Returns nothing.
-    def finish
-      end_para
-    end
-
-  private
-    # Updates the current Paragraph for this Block during the parsing.
-    #
-    # line - String line of text from the email.
-    #
-    # Returns nothing
-    def update_paragraph(line)
-      @line   += 1
-      stripped = line.sub(/^(\s|>)+/, '')
-      if stripped.size.zero?
-        end_para(@line-1)
-      else
-        if !@para
-          start_para
-          @para.is_signature = !!(stripped =~ /^[\-|_]/)
-        end
-        stripped.gsub! /\s[^\w\s]+\s/, ' '
-        stripped.gsub! /\s+/, ' '
-        stripped.downcase!
-        @para.update stripped
-      end
-    end
-
-    # Starts a new Paragraph during the parsing.
-    #
-    # Returns nothing.
-    def start_para
-      @paras << (@para = Paragraph.new(@line))
-    end
-
-    # Finishes the current Paragraph and adds its SHA1 hash to this Block's
-    # Set of SHAs.
-    #
-    # ending_line - String line of text from the email.
-    #
-    # Returns nothing.
-    def end_para(ending_line = @line)
-      return if !@para
-      @shas << @para.finish(ending_line).sha
-      @para = nil
-    end
-  end
-
-  # Tracks each paragraph of content in the email and builds up a SHA1 hash of
-  # the contents.  This SHA1 hash is used to determine if this paragraph is
-  # repeated in further emails in the current thread.  Paragraphs don't store
-  # the actual content, but contain start/end line numbers that match lines in
-  # the Paragraph's Block instance.
-  class Paragraph < Struct.new(:start, :end, :sha, :is_signature, :is_hidden)
-    def initialize(s = 0)
-      super
-      self.sha = Digest::SHA1.new
-      self.is_signature = false
-      self.is_hidden    = false
-    end
-
-    # Public: Gets whether this Paragraph is considered an email signature.
-    # Email signatures are just paragraphs that start with a leading dash or
-    # underscore.
-    #
-    # Returns true if this Paragraph is a signature, or false.
-    def signature?
-      is_signature
-    end
-
-    # Public: Gets whether this Paragraph is thought to be hidden.  This is
-    # determined after each line of text in the email has been processed.
-    #
-    # Returns true if this Paragraph is hidden, or false.
-    def hidden?
-      is_hidden
-    end
-
-    # Updates the SHA1 hash of this paragraph.
-    #
-    # s - String line of text from the email.
-    #
-    # Returns nothing.
-    def update(s)
-      self.sha.update(s)
-    end
-
-    # Sets the ending line of this Paragraph, and finalizes the SHA1 hash.
-    #
-    # ending_line - Fixnum representing which line in the Block this paragraph
-    #               ended on.
-    #
-    # Returns nothing.
-    def finish(ending_line)
-      self.end = ending_line
-      self.sha = self.sha.to_s
-      self
     end
   end
 end
--- test/email_reply_parser_test.rb
+++ test/email_reply_parser_test.rb
@@ -9,87 +9,62 @@ require dir + '..' + 'lib' + 'email_reply_parser'
 EMAIL_FIXTURE_PATH = dir + 'emails'
 
 class EmailReplyParserTest < Test::Unit::TestCase
-  def test_marks_nothing_hidden_on_a_single_reply_thread
-    reply = EmailReplyParser.read(email(:email_1_2))
-    assert reply.blocks.none? { |b|
-      b.paragraphs.any? { |para| para.hidden? } }
-  end
-
-  def test_marks_common_reply_portions_hidden_on_thread
-    reply1 = EmailReplyParser.read(email(:email_1_1))
-    reply2 = EmailReplyParser.read(email(:email_1_2), reply1.shas)
-    assert reply1.blocks.none? { |b|
-      b.paragraphs.any? { |para| para.hidden? } }
-    assert  reply2.blocks[4].hidden?
-    assert  reply2.blocks[3].hidden?
-    assert !reply2.blocks[2].hidden?
-  end
-
-  def test_marks_empty_block_between_two_hidden_blocks_as_hidden
-    reply1 = EmailReplyParser.read("abc def\n\n-signature")
-    reply2 = EmailReplyParser.read("wat\n\n>> abc def\n>\n\n-signature", reply1.shas)
-    assert_equal 4, reply2.blocks.size
-    assert reply2.blocks[3].hidden?
-    assert reply2.blocks[1].hidden?
-    assert reply2.blocks[2].hidden?
-  end
-
   def test_reads_simple_body
-    body   = email :email_1_1
-    blocks = EmailReplyParser::Reply.new(body).blocks
-    assert_equal 1, blocks.size
-    assert_match /Hi folks/, blocks.first.to_s
+    reply = email(:email_1_1)
+    assert_equal 3, reply.fragments.size
+
+    assert reply.fragments.none? { |f| f.quoted? }
+    assert_equal [false, true, true],
+      reply.fragments.map { |f| f.signature? }
+    assert_equal [false, true, true],
+      reply.fragments.map { |f| f.hidden? }
+
+    assert_equal "Hi folks
+
+What is the best way to clear a Riak bucket of all key, values after
+running a test?
+I am currently using the Java HTTP API.\n", reply.fragments[0].to_s
+
+    assert_equal "-Abhishek Kona\n\n", reply.fragments[1].to_s
   end
 
-  def test_reads_bottom_poster
-    body   = email :email_1_2
-    blocks = EmailReplyParser::Reply.new(body).blocks
-    assert_equal 5, blocks.size
-    assert_equal [0,1,0,1,0], blocks.map { |b| b.levels }
+  def test_reads_top_post
+    reply = email(:email_1_3)
+    assert_equal 5, reply.fragments.size
+
+    assert_equal [false, false, true, false, false],
+      reply.fragments.map { |f| f.quoted? }
+    assert_equal [false, true, true, true, true],
+      reply.fragments.map { |f| f.hidden? }
+    assert_equal [false, true, false, false, true],
+      reply.fragments.map { |f| f.signature? }
+
+    assert_match /^Oh thanks.\n\nHaving/, reply.fragments[0].to_s
+    assert_match /^-A/, reply.fragments[1].to_s
+    assert_match /^On [^\:]+\:/, reply.fragments[2].to_s
+    assert_match /^_/, reply.fragments[4].to_s
   end
 
-  def test_reads_multi_level_replies
-    body   = email :email_1_3
-    blocks = EmailReplyParser::Reply.new(body).blocks
-    assert_equal 7, blocks.size
-    assert_equal [0,1,2,1,2,1,0], blocks.map { |b| b.levels }
-  end
+  def test_reads_bottom_post
+    reply = email(:email_1_2)
+    assert_equal 6, reply.fragments.size
 
-  def test_stores_shas_of_blocks
-    body   = email :email_1_3
-    blocks = EmailReplyParser::Reply.new(body).blocks
-    assert_equal 19, blocks.inject(0) { |n, b| n + b.paragraphs.size }
-    assert_equal 1,  blocks[6].paragraphs.size
-    # check for the repeated mailing list footer
-    blocks[6].paragraphs.each do |para|
-      last = blocks[4].paragraphs.last
-      assert_equal para.sha, last.sha
-      assert last.include?(para.sha)
-      assert para.signature?, "only para of block 6 is not a signature"
-      assert last.signature?,
-        "matching sha in para 4 is not a signature"
-    end
-  end
+    assert_equal [false, true, false, true, false, false],
+      reply.fragments.map { |f| f.quoted? }
+    assert_equal [false, false, false, false, false, true],
+      reply.fragments.map { |f| f.signature? }
+    assert_equal [false, false, false, true, true, true],
+      reply.fragments.map { |f| f.hidden? }
 
-  def test_tracks_signature_blocks
-    body  = email :email_1_1
-    block = EmailReplyParser::Reply.new(body).blocks.first
-    assert_equal 4, block.paragraphs.size
-    block.paragraphs.each do |para|
-      if para.start == 6
-        assert_equal 6, para.end
-        assert para.signature?, "para on line 6 is not a signature"
-      elsif para.start == 9
-        assert_equal 12, para.end
-        assert para.signature?, "para on line 9 is not a signature"
-      else
-        assert !para.signature?, "para on line #{para.start} is a signature"
-      end
-    end
-    assert block.paragraphs.last.signature?
+    assert_equal "Hi,", reply.fragments[0].to_s
+    assert_match /^On [^\:]+\:/, reply.fragments[1].to_s
+    assert_match /^You can list/, reply.fragments[2].to_s
+    assert_match /^> /, reply.fragments[3].to_s
+    assert_match /^_/, reply.fragments[5].to_s
   end
 
   def email(name)
-    IO.read EMAIL_FIXTURE_PATH.join("#{name}.txt").to_s
+    body = IO.read EMAIL_FIXTURE_PATH.join("#{name}.txt").to_s
+    EmailReplyParser.read body
   end
 end
