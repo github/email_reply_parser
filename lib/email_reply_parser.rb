dc9c6af726a364d4e074dd849cd12ab6123e5021
Daniel Chatfield <chatfielddaniel@gmail.com>
2014-08-13 16:08:29 +0100

    Making regexp less greedy

--- lib/email_reply_parser.rb
+++ lib/email_reply_parser.rb
@@ -84,7 +84,7 @@ class EmailReplyParser
 
       # Check for multi-line reply headers. Some clients break up
       # the "On DATE, NAME <EMAIL> wrote:" line into multiple lines.
-      if text =~ /^(On\s(.+?)wrote:)$/nm
+      if text =~ /^(?!On.*On\s.+?wrote:)(On\s(.+?)wrote:)$/nm
         # Remove all new lines from the reply header.
         text.gsub! $1, $1.gsub("\n", " ")
       end
