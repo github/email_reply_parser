404b86eea76542c834fef4a779846561bd5ae579
Ari Summer <aribsummer@gmail.com>
2014-12-31 22:04:08 -0700

    Add From: phrase to email signature regex

--- lib/email_reply_parser.rb
+++ lib/email_reply_parser.rb
@@ -133,7 +133,7 @@ class EmailReplyParser
 
   private
     EMPTY = "".freeze
-    SIGNATURE = '(?m)(--\s*$|__\s*$|\w-$)|(^(\w+\s*){1,3} ym morf tneS$)'
+    SIGNATURE = '(?m)(--\s*$|__\s*$|\w-$)|(^(\w+\s*){1,3} ym morf tneS$)|(.*:morF)'
 
     begin
       require 're2'
