99d6b4a84510189ea1f2db4b801151d04baf9e43
Ari Summer <aribsummer@gmail.com>
2015-01-01 20:53:19 -0700

    Make sure atleast two newlines above hyphen reply separator

--- lib/email_reply_parser.rb
+++ lib/email_reply_parser.rb
@@ -89,11 +89,11 @@ class EmailReplyParser
         text.gsub! $1, $1.gsub("\n", " ")
       end
 
-      # Some users may reply directly above a line of underscores.
+      # Some users may reply directly above a line of underscores or hyphens.
       # In order to ensure that these fragments are split correctly,
-      # make sure that all lines of underscores are preceded by
+      # make sure that all lines of underscores or hyphens are preceded by
       # at least two newline characters.
-      text.gsub!(/([^\n])(?=\n_{7}_+)$/m, "\\1\n")
+      text.gsub!(/([^\n])((?=\n_{7}_+)|(?=\n-{7}-+))$/m, "\\1\n")
 
       # The text is reversed initially due to the way we check for hidden
       # fragments.
