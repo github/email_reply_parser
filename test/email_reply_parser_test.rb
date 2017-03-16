4acbf77ad373fdce835f73586dade1a7bab156e3
jgrau <jonas.grau@gmail.com>
2017-03-16 13:59:40 +0100

    Support gmail in danish quote header

--- test/email_reply_parser_test.rb
+++ test/email_reply_parser_test.rb
@@ -191,6 +191,11 @@ I am currently using the Java HTTP API.\n", reply.fragments[0].to_s
     assert_equal EmailReplyParser.read(body).visible_text, EmailReplyParser.parse_reply(body)
   end
 
+  def test_danish_gmail_client
+    body = IO.read EMAIL_FIXTURE_PATH.join("gmail_danish.txt").to_s
+    assert_equal 'This is text from gmail set up with danish locale', EmailReplyParser.parse_reply(body)
+  end
+
   def test_one_is_not_on
     reply = email("email_one_is_not_on")
     assert_match(/One outstanding question/, reply.fragments[0].to_s)
