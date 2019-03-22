59a09c98a9685b199f2f10f0d9e38b05c061b4f6
Aske Hansen <aske@deeco.dk>
2019-03-22 12:44:48 +0100

    accedentally removed EMPTY

--- lib/email_reply_parser.rb
+++ lib/email_reply_parser.rb
@@ -131,6 +131,7 @@ class EmailReplyParser
     end
 
   private
+    EMPTY = "".freeze
     SIGNATURE = '(?m)(--\s*$|__\s*$|\w-$)|(^(\w+\s*){1,3} ym morf tneS$)|(neslih gilnev deM$)|(sdrager dniK$)|(sdrager mraW$)|(sdrager tseB$)'
 
     begin
