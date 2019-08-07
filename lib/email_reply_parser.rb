f50d2dd6534baf4589b1e134a03bfd3c874b7874
Aaron Patterson <aaron.patterson@gmail.com>
2019-08-07 14:13:27 -0700

    bumping version

--- lib/email_reply_parser.rb
+++ lib/email_reply_parser.rb
@@ -30,7 +30,7 @@ require 'strscan'
 #
 # [mail]: https://github.com/mikel/mail
 class EmailReplyParser
-  VERSION = "0.5.9"
+  VERSION = "0.5.10"
 
   # Public: Splits an email body into a list of Fragments.
   #
